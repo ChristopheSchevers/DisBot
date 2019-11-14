fa42be9b537a23c8b6f44868c4f0ed7a1875e7cc
Christophe Schevers <christophe.schevers@gmail.com>
2019-11-14 21:16:20 +0100

    update

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./disconfig.json');
 const client = new Discord.Client();
+const ga = require('./gsAPI.js');
+
+ga.googleAPI();
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('message', message => {
-    console.log(message.content);
+
+    let msg = message.content;
+    
+    if(msg.substring(0,1) == '!') {
+        let args = msg.substring(1).split(' ');
+
+        console.log(args);
+    }
 });
 
 client.login(token);
