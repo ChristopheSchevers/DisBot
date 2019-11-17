a389e6497f8f1a27e56d72e07c75532dc248701a
Christophe Schevers <christophe.schevers@gmail.com>
2019-11-17 13:39:52 +0100

    Added update functionality and nodemon dependency

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('message', message => {
-    
+client.on('message', message => {    
     let msg = message.content;
     
-    if(msg.substring(0,1) == '!') {
+    if(msg.substring(0,1) == prefix) {
         let args = msg.substring(1).split(' ');
-        
-        if(args.length == 2) {
-            search(message, args);
+
+        if(args.length <= 6) {
+
+            if(args.length == 2) {
+                search(message, args);
+            } else if(args[0] == 'add') {
+                let reqArr = [];
+                reqArr.push(args.slice(1));
+                update(message, reqArr);
+            } else {
+                message.channel.send("I don't know this command.\nIn order to retrieve data from the table, please provide 2 argument after the exclamation mark.\nIf you want to add data to the table, please use command '!add' followed by 5 arguments");
+            }
+
+        } else {
+            message.channel.send("Oops! Your request seems too long. I only store data in 5 columns.");
         }
+        
     }
 });
 
@@ -44,14 +56,13 @@ function connect() {
 }
 
 async function search(message, args, client = googclient) {
+    const gsapi = google.sheets({version: 'v4', auth: client});   
 
-    const gsapi = google.sheets({version: 'v4', auth: client});
-    
     const opt = {
         spreadsheetId: '10j-LLoQIdnMLGFUoPfs0hyjUjl13lv1d3ilAsxoNFNE',
         range: 'Data!A2:E',
     };
-
+    
     let data = await gsapi.spreadsheets.values.get(opt);
     let dataArr = data.data.values;
     
@@ -63,5 +74,27 @@ async function search(message, args, client = googclient) {
     }
 
     message.channel.send(`No records were found with ${args[0]} ${args[1]}`);
+}
+
+async function update(message, args, client = googclient) {   
+
+    const gsapi = google.sheets({version: 'v4', auth: client});
+
+    const opt = {
+        spreadsheetId: '10j-LLoQIdnMLGFUoPfs0hyjUjl13lv1d3ilAsxoNFNE',
+        range: 'Data!A2:E',
+        valueInputOption: 'USER_ENTERED',
+        insertDataOption: 'INSERT_ROWS',
+        resource: { values: args }
+    };
+
+    await gsapi.spreadsheets.values.append(opt, function(err, res) {
+        if(err) {
+            console.log(err);
+            message.channel.send('An error has occurred. Please check the console or try again.')
+            return;
+        }
+        message.channel.send(`New row has been added to the table as col 1: ${args[0][0]}, col 2: ${args[0][1]}, min: ${args[0][2]}, max: ${args[0][3]}, avg: ${args[0][4]}`);
+    });
 
 }
